53b230fd2819b52e6d6534f4607f4ba029ca8f12
Liboq <2661158759@qq.com>
2022-04-19 11:36:43 +0800

    promise.any

---
+++ 字节飞书/code/11-promise.any.js
@@ -0,0 +1,15 @@
+const myPromiseAny=function(promises){
+  let arr=[]
+  let count =0
+  return new Promise((resolve,reject)=>{
+    promises.forEach((item,i) => {
+      Promise.resolve(item).then(resolve,err=>{
+        arr[i] = err
+        count ++
+        if(count ===promises.length) {
+          reject(new Error('没有promise执行成功'))
+        }
+      })
+    });
+  })
+}
